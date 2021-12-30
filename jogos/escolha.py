081a8f10f801652a7a96f445707621b534c3ec2a
felipe26 <felipexfe34@gmail.com>
2021-12-30 17:05:09 -0300

    modificaão

--- jogos/escolha.py
+++ jogos/escolha.py
@@ -1,11 +1,11 @@
-import jogo_da_velha
+import jogo_forca
 import jogo_sorte
-print("Escolha (1) jogo da sorte e (2) joga da velha")
+print("Escolha (1) jogo da sorte e (2) joga da forca")
 jogar = int(input("Qual jogo escolheu? "))
 
 if jogar == 1:
     jogo_sorte.sorte()
 elif jogar == 2:
-    jogo_da_velha.velha()
+    jogo_forca.velha()
 else:
     print("Deu erro ;(")
